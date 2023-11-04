5c09c3a4412c3a0b20e5d6a0807cc4c77e8279c2
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-11-04 20:34:19 -0300

    Adicionei o poema concreto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(@"u m
+     m o v i
+     mento
+   compondo
+  além
+         da
+ nuvem
+    um
+   campo
+      de
+   combate
+
+     mira
+   gem
+      ira
+         de
+    um
+      horizonte
+puro
+   num
+     mo
+     mento
+ vivo");
+ Console.ResetColor();
